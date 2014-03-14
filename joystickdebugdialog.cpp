0e7c78541181227c04441d9d6688406f065524c2
Luchs <Luchs@euirc.eu>
2014-03-14 12:58:33 +0100

    Use table to show Joystick values

--- joystickdebugdialog.cpp
+++ joystickdebugdialog.cpp
@@ -11,6 +11,14 @@ JoystickDebugDialog::JoystickDebugDialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    QTableWidgetItem *numHeader, *valHeader;
+    numHeader = new QTableWidgetItem("Number");
+    valHeader = new QTableWidgetItem("Value");
+    ui->axisTable->setHorizontalHeaderItem(0, numHeader);
+    ui->buttonTable->setHorizontalHeaderItem(0, numHeader);
+    ui->axisTable->setHorizontalHeaderItem(1, valHeader);
+    ui->buttonTable->setHorizontalHeaderItem(1, valHeader);
+
     Joystick *joystick = new Joystick(this);
     bool open = joystick->open(0);
     if (!open) {
@@ -28,12 +36,34 @@ JoystickDebugDialog::~JoystickDebugDialog()
 
 void JoystickDebugDialog::updateAxis(int num, int val)
 {
-    ui->axisNum->setText(QVariant(num).toString());
-    ui->axisVal->setText(QVariant(val).toString());
+    if (ui->axisTable->rowCount() < num)
+        ui->axisTable->setRowCount(num + 1);
+    QTableWidgetItem *numItem, *valItem;
+    numItem = ui->axisTable->item(num, 0);
+    valItem = ui->axisTable->item(num, 1);
+    if (!numItem) {
+        numItem = new QTableWidgetItem();
+        valItem = new QTableWidgetItem();
+        ui->axisTable->setItem(num, 0, numItem);
+        ui->axisTable->setItem(num, 1, valItem);
+    }
+    numItem->setText(QVariant(num).toString());
+    valItem->setText(QVariant(val).toString());
 }
 
 void JoystickDebugDialog::updateButton(int num, bool val)
 {
-    ui->buttonNum->setText(QVariant(num).toString());
-    ui->buttonVal->setText(QVariant(val).toString());
+    if (ui->buttonTable->rowCount() < num)
+        ui->buttonTable->setRowCount(num + 1);
+    QTableWidgetItem *numItem, *valItem;
+    numItem = ui->buttonTable->item(num, 0);
+    valItem = ui->buttonTable->item(num, 1);
+    if (!numItem) {
+        numItem = new QTableWidgetItem();
+        valItem = new QTableWidgetItem();
+        ui->buttonTable->setItem(num, 0, numItem);
+        ui->buttonTable->setItem(num, 1, valItem);
+    }
+    numItem->setText(QVariant(num).toString());
+    valItem->setText(QVariant(val).toString());
 }
