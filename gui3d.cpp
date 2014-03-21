4600d1005b7585f18d2092e4a6a9757b78747d3c
Sebastian Schmidt <isibboi@gmail.com>
2014-03-21 19:17:45 +0100

    Try to make green ball visible

--- gui3d.cpp
+++ gui3d.cpp
@@ -19,23 +19,20 @@ void Gui3D::initializeIrrlicht()
 {
     iw->initialize();
 
-    qDebug() << "Constructing gui3d";
     ISceneManager *sceneManager = iw->getSceneManager();
 
-    if (sceneManager != 0) {
-        qDebug() << "Got scene manager";
-    } else {
-        qDebug() << "Got NULL scene manager";
-    }
-
-    sceneManager->addCameraSceneNodeMaya();
+    sceneManager->getGUIEnvironment()->addStaticText(L"TestText", rect<s32>(10, 10, 200, 22));
+    sceneManager->addCameraSceneNode(0, vector3df(0, 0, 0), vector3df(1, 0, 0));
+    sceneManager->addLightSceneNode(0, vector3df(0, 0, 0), SColorf(1, 1, 1));
 
     const IGeometryCreator *geometryCreator = sceneManager->getGeometryCreator();
-    qDebug() << "Got geometry creator";
     sphere = geometryCreator->createSphereMesh();
     sphereNode = sceneManager->addMeshSceneNode(sphere);
 
-    sphereNode->setPosition(vector3df(0, 0, 0));
+    sphereNode->setPosition(vector3df(5, 0, 0));
+    ITexture *greenTexture = iw->getIrrlichtDevice()->getVideoDriver()->getTexture("textures/green.png");
+    sphereNode->setMaterialTexture(0, greenTexture);
+    sphereNode->setMaterialFlag(EMF_LIGHTING, false);
 
     qDebug() << "InitializedIrrlicht";
 }
