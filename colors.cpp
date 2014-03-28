0c854159d424ad138e78f0cc0f25a17bb63cff13
Luchs <Luchs@euirc.eu>
2014-03-28 07:21:35 -0400

    Fix color calculation
    Serialized hue is (actual hue / 2).

--- colors.cpp
+++ colors.cpp
@@ -5,7 +5,7 @@
 QColor parseColor(uint32_t color)
 {
     int h, s, v;
-    h = (color & 0xFF0000) >> 16;
+    h = ((color & 0xFF0000) >> 16) * 2;
     s = (color & 0xFF00) >> 8;
     v = color & 0xFF;
     return QColor::fromHsv(h, s, v);
@@ -15,7 +15,7 @@ uint32_t serializeColor(QColor color)
 {
     int h, s, v;
     color.getHsv(&h, &s, &v);
-    return (h << 16) |
+    return ((h / 2) << 16) |
            (s <<  8) |
            v;
 }
